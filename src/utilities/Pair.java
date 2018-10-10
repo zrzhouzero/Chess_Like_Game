1ca0bccef4143c377b6a632c33855ad2b3b79dde
周至柔 <zhouzhirou@zeros-mbp.gateway>
2018-10-10 19:39:52 +1100

    Changed the piece calculation rules on Board.java

--- src/utilities/Pair.java
+++ src/utilities/Pair.java
@@ -13,8 +13,10 @@ public class Pair {
     public int getLeft() { return this.L; }
     public int getRight() { return this.R; }
 
+/*
     public static boolean isSame(Pair pair1, Pair pair2) {
         return (pair1.getLeft() == pair2.getLeft()) && (pair1.getRight() == pair2.getRight());
     }
+*/
 
 }
